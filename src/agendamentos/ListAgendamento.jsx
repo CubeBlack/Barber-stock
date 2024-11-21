3a447e0e4f2b6ff67969ef32a4f452a39374aee3
Daniel Lima <deltaalfalambda@gmail.com>
2024-11-20 22:09:48 -0300

    Listar agendamentos da API

--- src/agendamentos/ListAgendamento.jsx
+++ src/agendamentos/ListAgendamento.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const ListAgendamento = () => {
+  const [data, setData] = useState([]); 
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    // Buscando os dados da API
+    axios.get(process.env.VITE_API_URL+'/agendamentos') 
+      .then(response => {
+        setData(response.data); 
+        setLoading(false); 
+      })
+      .catch(error => {
+        console.error('Erro ao buscar dados:', error);
+        setLoading(false); 
+      });
+  }, []); 
+
+  if (loading) {
+    return <div>Carregando...</div>; 
+  }
+
   return (
-    <div>
-      <h1>Agendamentos</h1>
-      <p>Welcome to the About page of the application.</p>
-    </div>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Data</th>
+          <th>Horario</th>
+          <th>Cliente</th>
+          <th>Obs</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.hora_inicio} à {item.hora_fim}</td>
+            <td>{(item.cliente).id} - {(item.cliente).nome}</td>
+            <td>{item.observacoes}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
